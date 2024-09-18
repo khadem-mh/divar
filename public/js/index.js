0be80063baf526d66317ebf28ed5f45ccff80aa0
khadem-mh <khadem13359@gmail.com>
2024-09-18 13:49:33 -0700

    current finished

--- public/js/index.js
+++ public/js/index.js
@@ -7,11 +7,52 @@ window.addEventListener('load', () => {
     getAllCities()
         .then(res => {
 
+            const popularCitiesContainer = selectElem("#popular-cities")
+            const searchInput = selectElem("#search-input")
+            const searchResult = selectElem(".search-result-cities")
+            const cities = res.data.cities
+
+            //Hidden Loading
             loadingContainer.style.display = 'none'
 
-            const popularCitiesContainer = selectElem("#popular-cities")
+            //Handle Logic Searching
+            searchInput.addEventListener('keyup', event => {
 
-            const popularCities = res.data.cities.filter(city => city.popular)
+                const val = event.target.value
+                searchResult.innerHTML = ""
+
+                if (val.length) {
+
+                    searchResult.classList.add("active")
+
+                    const searchResultCities = cities.filter(city => city.name.startsWith(val))
+
+                    if (searchResultCities.length) {
+
+                        searchResultCities.forEach(city => {
+                            insertElemToDom(
+                                searchResult,
+                                `<li>${city.name}</li>`
+                            )
+                        })
+
+                    } else
+                        insertElemToDom(
+                            searchResult,
+                            `
+                            <img src="https://support-faq.divarcdn.com/web/2024/03/static/media/magnifier.7f88b2e3f8ae30f4333986d0b0fbcf1d.svg" />
+                            <p class="empty">نتیجه‌ای برای جستجوی شما پیدا نشد.</p>
+                            `
+                        )
+
+                } else
+                    searchResult.classList.remove("active")
+
+
+            })
+
+            //Show Popular Cities
+            const popularCities = [...cities].filter(city => city.popular)
 
             popularCities.forEach(city => {
 

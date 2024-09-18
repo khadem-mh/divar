78e0fd16d8d2f939cd8744c384583f1411babc41
khadem-mh <khadem13359@gmail.com>
2024-09-18 11:31:58 -0700

    get all cities and show popular cities form DOM

--- public/js/index.js
+++ public/js/index.js
@@ -1,64 +1,28 @@
-import { fetchCitiesPopular, setCityCookie, getCityCookie, fetchCities } from "./utils/cities.js"
+import { getAllCities, selectElem, insertElemInDom } from "../../utils/shared.js"
 
-// var dom
-const $ = document
-const citiesPopularList = $.querySelector('.main__cities-list .row')
-const citySearchInput = $.querySelector('.main__input')
-const citiesSearchResult = $.querySelector('.search-result-cities')
-const fragmentCities = $.createDocumentFragment()
-let cities = null
+window.addEventListener('load', () => {
 
-//! Funcs Helper
-const cityClickHandler = (event, city) => {
-    event.preventDefault()
-    setCityCookie(city)
-    window.location.href = `${window.location.origin}/frontend/pages/main.html?city=${city}`
-}
+    getAllCities()
+        .then(res => {
+            const popularCitiesContainer = selectElem(".main__cities-list")
 
-const showPopularCities = cities => {
-    citiesPopularList.innerHTML = ''
-    cities.map(city => {
-        const divElem = $.createElement('div')
-        divElem.className = 'col-2 d-flex justify-content-center'
-        divElem.innerHTML = `  
-            <li class="main__cities-item">
-                <a class="main__cities-link" href="#" onclick="cityClickHandler(event, '${city.href}')">${city.name}</a>
-            </li>
-        `
-        fragmentCities.appendChild(divElem)
-    })
-    citiesPopularList.appendChild(fragmentCities)
-}
+            const popularCities = res.data.cities.filter(city => city.popular)
 
+            popularCities.forEach(city => {
 
-//! Functions Rendering DOM
-const citySearchHandler = value => {
-    let citiesFilter = null
-    if (value) {
-        citiesSearchResult.className = 'search-result-cities active'
-        citiesFilter = [...cities].filter(city => city.name.startsWith(value))
-        console.log('cityFilter =>>', citiesFilter);
-        citiesSearchResult.innerHTML = ''
-        citiesFilter.forEach(city => {
-            citiesSearchResult.insertAdjacentHTML('beforeend', `<li>${city.name}</li>`)
+                insertElemInDom(
+                    popularCitiesContainer.firstElementChild,
+                    `
+                    <div class="col-2 d-flex justify-content-center">
+                        <li class="main__cities-item">
+                            <a class="main__cities-link" href="#">${city.name}</a>
+                        </li>
+                    </div>
+                    `
+                )
+
+            })
+
         })
-        citiesFilter = ''
-    }
-    else citiesSearchResult.className = 'search-result-cities'
 
-}
-
-
-//! Binding
-window.cityClickHandler = cityClickHandler
-
-
-//! EventListener
-window.addEventListener('keyup', event => citySearchHandler(event.target.value))
-window.addEventListener('load', async () => {
-    const citiesPopular = await fetchCitiesPopular()
-    showPopularCities(citiesPopular)
-    cities = await fetchCities()
-    let cityNmae = getCityCookie()
-    cityNmae !== undefined && (window.location.href = `${window.location.origin}/frontend/pages/main.html?city=${cityNmae}`)
 })
---
+++ utils/shared.js
@@ -0,0 +1,11 @@
+export const baseURLApi = "https://divarapi.liara.run/v1/"
+
+// Get All Cities
+export const getAllCities = async () => {
+    const res = await fetch(`${baseURLApi}location`)
+    return await res.json()
+}
+
+export const selectElem = param => document.querySelector(param)
+
+export const insertElemInDom = (parent, elem) => parent.insertAdjacentHTML("beforeend", elem)

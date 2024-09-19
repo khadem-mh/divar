4b7bd45d5c3e5fda2e6ddda35d3f9343dc09eccf
khadem-mh <khadem13359@gmail.com>
2024-09-19 12:11:40 -0700

    fix bug posts.html page for hidden loading while res recived

--- public/js/posts.js
+++ public/js/posts.js
@@ -1,11 +1,18 @@
-import { getCityAdvertisments, hiddenLoading } from "../../utils/shared"
+import { getCityAdvertisments } from "../../utils/shared.js"
+import { hiddenLoading } from "../../utils/utils.js";
 
 window.addEventListener('load', () => {
 
+    console.log("opk");
+    
+
     getCityAdvertisments().then(res => {
+        console.log(res);
 
         //Hidden Loading
         hiddenLoading()
+
+        
 
     })
     

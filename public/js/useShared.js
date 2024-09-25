1dab7e4e2743488c194f7faef2220e2174d326f7
khadem-mh <khadem13359@gmail.com>
2024-09-24 22:46:21 -0700

    handle logic clear search input

--- public/js/useShared.js
+++ public/js/useShared.js
@@ -1,10 +1,29 @@
 import { getSocialsAndShow } from "../../utils/shared.js";
-import { addParamToURL, selectElem } from "../../utils/utils.js";
+import { addParamToURL, getUrlParam, removeParamFromUrl, selectElem } from "../../utils/utils.js";
 
 window.addEventListener('load', (e) => {
     getSocialsAndShow()
 
+    // Global Search Handler
     const inpElem = selectElem("#global_search_input")
+    const inpElemCls = selectElem("#remove-search-value-icon")
+    let urlVal = ""
+
+    if (location.search.includes("value")) {
+
+        urlVal = getUrlParam("value")
+        inpElemCls.style.display = "block"
+
+        inpElemCls.addEventListener("click", () => {
+
+            inpElemCls.style.display = "none"
+            inpElem.value = ""
+            removeParamFromUrl("value")
+        })
+
+    }
+
+    inpElem.value = urlVal && urlVal
 
     const searchGlobalHandler = e => {
         let val = e.target.value.trim()
